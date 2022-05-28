6d8ffdb03ace8a2049d8a461681b70d13da8c9ee
Tarun Bhardwaj 1901920100297 <taruncsgn@gmail.com>
2022-05-28 15:28:53 +0530

    production ready

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -4,37 +4,42 @@ import Image from "next/image";
 
 function Card(props) {
   return (
-    <div className={styles.card}>
-      <Image src={props.info.map_url} alt="map" width={296} height={148} />
-      <div className={styles.rideDetails}>
-        <h3 className={styles.detailField}>
-          Ride Id: <span className={styles.detailEntry}>{props.info.id}</span>
-        </h3>
-        <h3 className={styles.detailField}>
-          Origin Station:{" "}
-          <span className={styles.detailEntry}>
-            {props.info.origin_station_code}
-          </span>
-        </h3>
-        <h3 className={styles.detailField}>
-          Station Path:{" "}
-          <span className={styles.detailEntry}>
-            {props.info.station_path.toString()}
-          </span>
-        </h3>
-        <h3 className={styles.detailField}>
-          Date: <span className={styles.detailEntry}>{new Date(props.info.date).toLocaleString()}</span>
-        </h3>
-        <h3 className={styles.detailField}>
-          Distance:{" "}
-          <span className={styles.detailEntry}>{props.info.distance}</span>
-        </h3>
+    props.info != null && (
+      <div className={styles.card}>
+        <Image src={props.info.map_url} alt="map" width={296} height={148} />
+        <div className={styles.rideDetails}>
+          <h3 className={styles.detailField}>
+            Ride Id: <span className={styles.detailEntry}>{props.info.id}</span>
+          </h3>
+          <h3 className={styles.detailField}>
+            Origin Station:{" "}
+            <span className={styles.detailEntry}>
+              {props.info.origin_station_code}
+            </span>
+          </h3>
+          <h3 className={styles.detailField}>
+            Station Path:{" "}
+            <span className={styles.detailEntry}>
+              {props.info.station_path.toString()}
+            </span>
+          </h3>
+          <h3 className={styles.detailField}>
+            Date:{" "}
+            <span className={styles.detailEntry}>
+              {new Date(props.info.date).toLocaleString()}
+            </span>
+          </h3>
+          <h3 className={styles.detailField}>
+            Distance:{" "}
+            <span className={styles.detailEntry}>{props.info.distance}</span>
+          </h3>
+        </div>
+        <div className={styles.locationDetails}>
+          <h3>{props.info.city + ",  "}</h3>
+          <h3>{props.info.state}</h3>
+        </div>
       </div>
-      <div className={styles.locationDetails}>
-        <h3>{props.info.city+",  "}</h3>
-        <h3>{props.info.state}</h3>
-      </div>
-    </div>
+    )
   );
 }
 
--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -5,19 +5,21 @@ import Image from "next/image";
 function Nav(props) {
   console.log(props);
   return (
-    <div className={styles.nav}>
-      <h1 className={styles.title}>Edvora</h1>
-      <div className={styles.userInfo}>
-        <Image
-          className={styles.avatar}
-          src={props.userInfo.url}
-          alt="Avatar"
-          width={44}
-          height={44}
-        />
-        <span className={styles.userName}>{props.userInfo.name}</span>
+    props.userInfo != null && (
+      <div className={styles.nav}>
+        <h1 className={styles.title}>Edvora</h1>
+        <div className={styles.userInfo}>
+          <Image
+            className={styles.avatar}
+            src={props.userInfo.url}
+            alt="Avatar"
+            width={44}
+            height={44}
+          />
+          <span className={styles.userName}>{props.userInfo.name}</span>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
